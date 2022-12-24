f8b9a441b0f2ba63db29f017604243ee74fde431
James Richie <james.richie.sulaeman@gmail.com>
2022-12-23 21:20:55 -0800

    Added setup.sh which prevents Git from updating the skeleton credentials folder

---
+++ setup.sh
@@ -0,0 +1,3 @@
+git update-index --skip-worktree src/main/resources/credentials/dbconn.credentials
+git update-index --skip-worktree src/main/resources/credentials/jwt.credentials
+git update-index --skip-worktree src/main/resources/credentials/smtp.credentials
