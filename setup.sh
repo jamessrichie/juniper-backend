db0a6725de85a6e77a836c9fd75e567c8a4b7554
James Richie <james.richie.sulaeman@gmail.com>
2023-08-11 20:51:14 -0700

    Updated dependencies and setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
-git update-index --skip-worktree src/main/resources/credentials/dbconn.credentials
+git update-index --skip-worktree src/main/resources/credentials/database.credentials
 git update-index --skip-worktree src/main/resources/credentials/jwt.credentials
 git update-index --skip-worktree src/main/resources/credentials/smtp.credentials
+git update-index --skip-worktree src/main/resources/credentials/storage.credentials
